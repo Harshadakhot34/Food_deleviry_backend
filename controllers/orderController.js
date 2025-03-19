498368cbecc921234e75086a8f28f834d37fde48
Harshadakhot34 <hkhot30@gmail.com>
2025-03-19 19:39:13 +0530

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -6,7 +6,9 @@ const STRIPE_SECRET_KEY =
   "sk_test_51R1lM3QOdyf66dQdlODTDWirIJsdGsLJG5sD2mNxFnYixmD0Tftp4AmpaAMNXQ03MZV2egtnD4KFvRvZONLIiban00nNhQUGiL";
 
 // const frontend_url = "http://localhost:5173";
-const frontend_url = "https://food-delivery-backend-flws.onrender.com";
+// const frontend_url = "https://food-delivery-backend-flws.onrender.com";
+  const frontend_url = "https://yumfast.netlify.app"; 
+
 const stripe = new Stripe(STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
